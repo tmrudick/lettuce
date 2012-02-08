54c1f00ff66d65bfaea2b7820a79b4e39725709a
Tom Rudick <tmrudick@gmail.com>
2012-02-07 21:09:48 -0800

    Adding foodtv test case

--- test/test_lettuce.rb
+++ test/test_lettuce.rb
@@ -14,4 +14,12 @@ class LettuceTest < Test::Unit::TestCase
   def test_url_with_no_hrecipe
     assert_equal [], Lettuce.parse_url("http://github.com/tmrudick/lettuce")
   end
+  
+  def test_foodtv
+    recipe = Lettuce.parse_url("fixtures/asparagus.html")
+    
+    assert_equal "Roasted Asparagus", recipe.fn
+    assert_equal 5, recipe.ingredients.size
+    assert_equal "http://img.foodnetwork.com/FOOD/2009/01/13/vday_roastedasparagus4854_s4x3_med.jpg", recipe.photo
+  end
 end
