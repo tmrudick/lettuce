84112970246d40c5398e67fa28231fe85b213f3b
Tom Rudick <tmrudick@gmail.com>
2012-02-07 21:43:04 -0800

    Fixing test failures

---
+++ test/helper.rb
@@ -0,0 +1,3 @@
+def html_test_filename(filename)
+  File.join(File.dirname(__FILE__), 'fixtures', filename)
+end
--- test/test_lettuce.rb
+++ test/test_lettuce.rb
@@ -1,5 +1,6 @@
 require 'test/unit'
 require 'lettuce'
+require File.join(File.dirname(File.absolute_path(__FILE__)),'helper')
 
 class LettuceTest < Test::Unit::TestCase
 
@@ -12,14 +13,15 @@ class LettuceTest < Test::Unit::TestCase
   end
   
   def test_url_with_no_hrecipe
-    assert_equal [], Lettuce.parse_url("http://github.com/tmrudick/lettuce")
+    assert_equal [], Lettuce.parse_url("http://www.tomrudick.com")
   end
   
   def test_foodtv
-    recipe = Lettuce.parse_url("fixtures/asparagus.html")
+    recipes = Lettuce.parse_url(html_test_filename("asparagus.html"))
     
-    assert_equal "Roasted Asparagus", recipe.fn
-    assert_equal 5, recipe.ingredients.size
-    assert_equal "http://img.foodnetwork.com/FOOD/2009/01/13/vday_roastedasparagus4854_s4x3_med.jpg", recipe.photo
+    assert_equal 1, recipes.size
+    assert_equal "Roasted Asparagus", recipes[0].fn
+    assert_equal 5, recipes[0].ingredients.size
+    assert_equal "http://img.foodnetwork.com/FOOD/2009/01/13/vday_roastedasparagus4854_s4x3_med.jpg", recipes[0].photo
   end
 end
