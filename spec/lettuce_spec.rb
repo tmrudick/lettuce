3fa459f990dab072993cfcfd531ed5a427795dc6
Tom Rudick <tmrudick@gmail.com>
2012-02-14 20:49:42 -0800

    Adding tests

--- spec/lettuce_spec.rb
+++ spec/lettuce_spec.rb
@@ -8,4 +8,8 @@ describe Lettuce do
   it 'should return nil if no recipes are found' do
     Lettuce.parse("http://tomrudick.com").should == nil
   end
+  
+  it 'should return nil if it gets a bad url' do
+    Lettuce.parse("this is not a URL I don't even").should == nil
+  end  
 end
