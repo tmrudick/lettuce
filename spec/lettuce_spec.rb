3433a44f7956620a83b874c7621b6da96e1d2a02
Tom Rudick <tmrudick@gmail.com>
2012-02-15 00:45:20 -0800

    Overriding OpenURI::OpenRead#open to point to disk instead of web

--- spec/lettuce_spec.rb
+++ spec/lettuce_spec.rb
@@ -1,21 +1,36 @@
 require 'lettuce'
+require 'open-uri'
+require 'uri'
 
 describe Lettuce do
-  before do
-    # Set up fixture stuff
+  before(:all) do
+    module OpenURI
+      module OpenRead
+        def open(*rest, &block)          
+          case self.to_s
+          when "http://www.foodnetwork.com/recipes/tyler-florence/roasted-asparagus-recipe/index.html"
+            url = File.join(File.dirname(__FILE__), "fixtures/asparagus.html")
+          else
+            url = File.join(File.dirname(__FILE__), "fixtures/default.html")
+          end
+
+          open_uri_original_open(url, *rest, &block)
+        end
+      end
+    end    
   end
 
   it 'should return nil if no recipes are found' do
     Lettuce.parse("http://tomrudick.com").should == nil
   end
-  
+
   it 'should return nil if it gets a bad url' do
     Lettuce.parse("this is not a URL I don't even").should == nil
   end  
-  
+
   it 'should return a recipe object called Roasted Asparagus' do
     recipe = Lettuce.parse("http://www.foodnetwork.com/recipes/tyler-florence/roasted-asparagus-recipe/index.html")
-    
+
     recipe.title.should == "Roasted Asparagus"
   end
 end
