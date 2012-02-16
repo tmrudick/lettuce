a31ae16da139cfbc952078bff6fce568bd98fc96
Tom Rudick <tmrudick@gmail.com>
2012-02-15 22:28:26 -0800

    Adding class specific tests

---
+++ spec/all_recipes_spec.rb
@@ -0,0 +1,25 @@
+require 'lettuce'
+
+describe Lettuce::AllRecipes do
+  before(:all) do
+   @file = open(File.join(File.dirname(__FILE__), "fixtures/pineapple_rolls.html"))
+   @document = Nokogiri::HTML(@file)
+
+   @url = "http://allrecipes.com/Recipe/Pineapple-Sweet-Rolls/Detail.aspx"
+  end
+
+  it 'should be able to parse this recipe' do
+    Lettuce::AllRecipes.can_parse?(@document, @url)
+  end
+
+  it 'should be called Pineapple Sweet Rolls' do
+    recipe = Lettuce::AllRecipes.new(@document, @url)
+    
+    recipe.title.should == "Pineapple Sweet Rolls"
+  end
+
+  it 'should return 15 ingredients' do
+    recipe = Lettuce::AllRecipes.new(@document, @url)
+    recipe.ingredients.size.should == 15    
+  end
+end
---
+++ spec/hrecipe_spec.rb
@@ -0,0 +1,25 @@
+require 'lettuce'
+
+describe Lettuce::HRecipe do
+  before(:all) do
+   @file = open(File.join(File.dirname(__FILE__), "fixtures/asparagus.html"))
+   @document = Nokogiri::HTML(@file)
+
+   @url = "http://www.foodnetwork.com/recipes/tyler-florence/roasted-asparagus-recipe/index.html"
+  end
+
+  it 'should be able to parse this recipe' do
+    Lettuce::HRecipe.can_parse?(@document, @url)
+  end
+
+  it 'should be called Roasted Asparagus' do
+    recipe = Lettuce::HRecipe.new(@document, @url)
+    
+    recipe.title.should == "Roasted Asparagus"
+  end
+
+  it 'should return only 6 ingredients' do
+    recipe = Lettuce::HRecipe.new(@document, @url)
+    recipe.ingredients.size.should == 5    
+  end
+end
