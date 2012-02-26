22db285c34539127c23e782dfa6d861857c71b35
Tom Rudick <tmrudick@gmail.com>
2012-02-26 15:17:22 -0800

    Adding all hRecipe properties

--- lib/lettuce/hrecipe/hrecipe.rb
+++ lib/lettuce/hrecipe/hrecipe.rb
@@ -1,6 +1,5 @@
 module Lettuce
   class HRecipe
-    
     class << self
       def can_parse?(doc, url)
         doc.css('.hrecipe').size > 0
@@ -17,7 +16,12 @@ module Lettuce
         else 
           nil
         end
-      end
+      end 
+      
+      def parse_all(doc, url)
+        root = get_root(doc)
+        root.collect { |node| self.new(node) }
+      end     
     end
     
     attr_reader :url, :title, :ingredients
@@ -31,16 +35,43 @@ module Lettuce
     end
     
     def ingredients
-      ingredients_list = []
-      @root.css('.ingredient').each do |ingredient|
-        ingredients_list << ingredient.content.strip
-      end
-      
-      ingredients_list
+      @root.css('.ingredient').collect { |ingredient| ingredient.content.strip }
     end
     
     def photo
       @root.css('.photo')[0]['src']
+    end
+    
+    def yield
+      @root.css('.yield')[0].content.strip
+    end
+    
+    def instructions
+      @root.css('.instructions')[0].content.strip
+    end
+    
+    def duration
+      @root.css('.duration').collect { |duration| duration.content.strip }
+    end
+    
+    def summary
+      @root.css('.summary').content.strip
+    end
+    
+    def author
+      @root.css('.author').collect { |author| author.content.strip }
+    end
+    
+    def published
+      @root.css('.published')      
+    end
+    
+    def nutrition
+      @root.css('.nutrition').collect { |nutrition| nutrition.content.strip }
+    end
+    
+    def tags
+      @root.css('.tag').collect { |tag| tag.content.strip }
     end
   end
 end
